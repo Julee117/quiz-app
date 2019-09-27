98adac49dff26b10d9ab874faa3ef83fd5840c03
github username <juliannne@gmail.com>
2019-09-27 15:58:27 -0400

    Add validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,4 +4,8 @@ class User < ApplicationRecord
   has_many :quizzes, through: :scores
 
   has_secure_password
+  validates :first_name, presence: true
+  validates :last_name, presence: true
+  validates :email, presence: true
+  validates :email, uniqueness: true
 end
