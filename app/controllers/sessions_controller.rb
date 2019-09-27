c7c728be22bb7ed96c58ea5b4f82b634e50ab7cd
github username <juliannne@gmail.com>
2019-09-27 16:07:05 -0400

    Add new, create and destroy methods

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,2 +1,24 @@
 class SessionsController < ApplicationController
+  def new
+  end
+
+  def create
+    @user = User.find_by(email: params[:email])
+    if @user && @user.authenticate(params[:password])
+      session[:user_id] = @user.id
+      redirect_to users_path
+    else
+      flash[:alert] = "Invalid email/password"
+      render :new
+    end
+  end
+
+  def destroy
+    if session[:user_id]
+      session.delete(:user_id)
+      redirect_to "/login"
+    else
+      redirect_to users_path
+    end
+  end
 end
