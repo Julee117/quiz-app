8f6722786203eb6c8771c5390b62b596fd3794a6
github username <juliannne@gmail.com>
2019-09-27 15:56:01 -0400

    Add new, create and user_params methods

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,26 @@
 class UsersController < ApplicationController
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.create(user_params)
+
+    respond_to do |format|
+      if @user.save
+        session[:user_id] = @user.id
+        format.html { redirect_to users_path }
+        format.json { render json: @user, status: 201 }
+      else
+        format.html { render :new }
+        format.json { render json: @user.errors, status: 422 }
+      end
+    end
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:first_name, :last_name, :email, :password, :password_confirmation)
+  end
 end
