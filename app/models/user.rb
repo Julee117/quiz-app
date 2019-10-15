ed95e48f95fe20aed6bbd269ce45c763ad018926
github username <juliannne@gmail.com>
2019-10-15 18:56:18 -0400

    Add get_score and add_attribute_value methods

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,4 +8,25 @@ class User < ApplicationRecord
   validates :last_name, presence: true
   validates :email, presence: true
   validates :email, uniqueness: true
+
+  def get_score(choices)
+    score = 0
+    answers = []
+    choices.values.each do |choice|
+      option = Choice.find(choice.to_i)
+      option.update_attribute(:user_id, self.id)
+      answers << option
+    end
+    answers.each do |answer|
+      if answer.correct
+        score += 1
+      end
+    end
+    score = (score.to_f / (choices.keys.length)) * 100
+    return score
+  end
+
+  def add_attribute_value(id)
+    self.update_attribute(:topic, id)
+  end
 end
