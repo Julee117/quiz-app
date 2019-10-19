a18bea7dcef45488c57df014d50b5c47c3ef4a43
github username <juliannne@gmail.com>
2019-10-19 01:42:10 -0400

    Add helper method

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,6 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
-  helper_method :current_user, :logged_in?
+  helper_method :current_user, :logged_in?, :is_admin?
 
   def logged_in?
     !!current_user
@@ -8,5 +8,9 @@ class ApplicationController < ActionController::Base
 
   def current_user
     @user ||= User.find(session[:user_id]) if session[:user_id]
+  end
+
+  def is_admin?
+    logged_in? && current_user.admin?
   end
 end
