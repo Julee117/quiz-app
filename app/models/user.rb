d13e534b4bbfa12b25f87e8a3f6504886c2d9a5d
github username <juliannne@gmail.com>
2019-10-19 01:38:21 -0400

    Add get_user_answers and user_quiz_result methods

--- app/models/user.rb
+++ app/models/user.rb
@@ -29,4 +29,26 @@ class User < ApplicationRecord
   def add_attribute_value(id)
     self.update_attribute(:topic, id)
   end
+
+  def get_user_answers(id)
+    answers = []
+    quiz = Quiz.find(id)
+    quiz.questions.each do |question|
+      question.choices.each do |choice|
+        if choice.user_id == self.id
+          answers << choice
+        end
+      end
+    end
+    return user_quiz_result(answers)
+  end
+
+  def user_quiz_result(answers)
+    response = {}
+    answers.each do |answer|
+      question = Question.find(answer.question_id)
+      response[question.content] = answer
+    end
+    return response
+  end
 end
