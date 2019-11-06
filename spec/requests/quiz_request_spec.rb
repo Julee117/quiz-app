366b1f1ff2701f20f731f2e504d0cd3963a56bc9
github username <juliannne@gmail.com>
2019-11-06 16:55:05 -0500

    Add test for fetching single quiz

--- spec/requests/quiz_request_spec.rb
+++ spec/requests/quiz_request_spec.rb
@@ -2,12 +2,12 @@ require 'rails_helper'
 
 RSpec.describe 'Quizzes API', type: :request do
   let!(:quizzes) { create_list(:quiz, 5) }
+  let(:quiz_id) { quizzes.first.id }
 
   describe 'GET /quizzes' do
     before { get '/quizzes.json' }
 
     it 'returns quizzes' do
-      json = JSON.parse(response.body)
       expect(json).not_to be_empty
       expect(json.size).to eq(5)
     end
@@ -80,7 +80,6 @@ RSpec.describe 'Quizzes API', type: :request do
     context 'when quiz is successfully created' do
       before { post "/quizzes.json", params: valid_attributes }
       it 'creates a quiz' do
-        json = JSON.parse(response.body, symbolize_names: true)
         expect(json[:name]).to eq(valid_attributes[:quiz][:name])
         expect(json[:id]).not_to eq(nil)
       end
@@ -101,6 +100,21 @@ RSpec.describe 'Quizzes API', type: :request do
       before { post "/quizzes", params: invalid_attributes }
       it 'renders create quiz page' do
         expect(response).to render_template(:new)
+      end
+    end
+  end
+
+  describe 'GET /quizzes/:id' do
+    context 'when quiz exists' do
+      before { get "/quizzes/#{quiz_id}.json" }
+      it 'returns a status code of 200' do
+        expect(response).to have_http_status(200)
+      end
+
+      it 'returns the quiz' do
+        expect(json).not_to be_empty
+        expect(json[:id]).to eq(quiz_id)
+        expect(json[:name]).to eq(quizzes.first.name)
       end
     end
   end
